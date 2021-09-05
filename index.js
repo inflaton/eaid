4e3a68ab17552a437593993041ff07fbea30a094
Donghao Huang <donghao.huang@mastercard.com>
2021-09-06 07:41:45 +0800

    changed to ES modules

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { generateEaid } = require("./lib/eaid.js");
+import { generateEaidList } from "./lib/eaid.js";
 
 const args = process.argv.slice(2);
 
@@ -7,15 +7,30 @@ const rangeId = args[1] || 1;
 const issuerAssignedNumberMin = args[2] || 1;
 const issuerAssignedNumberMax = args[3] || 125;
 
-const timerTitle = `Time taken to generate ${
+let timerTitle = `Time taken to generate ${
   issuerAssignedNumberMax - issuerAssignedNumberMin + 1
-} EAIDs`;
+} EAIDs - w/ output`;
 console.time(timerTitle);
+generateEaidList(
+  issuerId,
+  rangeId,
+  issuerAssignedNumberMin,
+  issuerAssignedNumberMax,
+  true
+);
 
-console.log("#index,EAID");
-for (let i = issuerAssignedNumberMin; i <= issuerAssignedNumberMax; i++) {
-  const eaid = generateEaid(issuerId, rangeId, i);
-  console.log(`${i},${eaid}`);
-}
+console.timeEnd(timerTitle);
+
+timerTitle = `Time taken to generate ${
+  issuerAssignedNumberMax - issuerAssignedNumberMin + 1
+} EAIDs - w/o output`;
+console.time(timerTitle);
+generateEaidList(
+  issuerId,
+  rangeId,
+  issuerAssignedNumberMin,
+  issuerAssignedNumberMax,
+  false
+);
 
 console.timeEnd(timerTitle);
